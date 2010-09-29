6d0a14fb3e9421f943e4bb6eb4729f6098680d05
Thordur Arnarson <thordurba08@ru.is>
2010-09-29 13:01:29 +0000

    Fyrst commit

---
+++ src/is/ru/aod/fmal/main.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package is.ru.aod.fmal;
+
+/**
+ * @author Thordur Arnarson
+ *
+ */
+public class main {
+	public static void main(String args[]){
+		System.out.println("Hello Hallo man");
+	}
+}
